726ed586ae226bdadd22dd0a8847160388d91fdf
Francesca-Barcella <pollon.fra@gmail>
2022-10-20 14:31:34 +0200

    JS - OT - milestone 0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,60 @@
-alert('ciao, funziono!')
+//alert('ciao, funziono!')
+
+/******************************************************************************** 
+TRACCIA 
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0: Creare l’array di oggetti con le informazioni fornite
+MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1: Trasformare la stringa foto in una immagine effettiva
+BONUS 2: Organizzare i singoli membri in card/schede
+********************************************************************************/
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// proprietà da inserire: nome, ruolo, foto.
+
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite
+const membersTeam = [
+    {
+        Name: 'Wayne Barnett',
+        Role: 'Founder & CEO',
+        Image: './assets/img/wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        Name: 'Angela Caroll',
+        Role: 'Chief Editor',
+        Image: './assets/img/angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        Name: 'Waleter gordon',
+        Role: 'Social Media Manager',
+        Image: './assets/img/walter-gordon-office-manager.jpg',
+    },
+
+    {
+        Name: 'Angela Barnett',
+        Role: 'Chief Editor',
+        Image: './assets/img/wayne-barnett-founder-ceo.jpg',
+    },
+
+    {
+        Name: 'Scott Estrada',
+        Role: 'Developer',
+        Image: './assets/img/scott-estrada-founder-ceo.jpg',
+    },
+
+    {
+        Name: 'Barbara Ramos',
+        Role: 'Grafic designer',
+        Image: './assets/img/barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+console.log(membersTeam);
+
+
+
+
